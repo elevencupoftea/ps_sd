f79eb8a6b2db39171d9007f9f2ebd48bb674ac25
elevencupoftea <elevencupoftea@gmail.com>
2023-09-15 09:28:45 +0300

    Python 3.10.6, custom venv

--- install.sh
+++ install.sh
@@ -3,15 +3,16 @@
 PROJECT_PATH="Stable"
 SD_PATH="stable_diffusion"
 NOTEBOOK_PATH=$(pwd)
-VENV_PATH=$NOTEBOOK_PATH/venv
+VENV_PATH=$PROJECT_PATH/venv
 
 # Update system
 function system_update() {
   sudo apt update
-  sudo apt install wget git python3 python3-venv python3-pip -y
+  sudo apt install wget git -y
 }
 
 function prepare_environment() {
+  # Make project folder
   mkdir $PROJECT_PATH
   cd $PROJECT_PATH || exit
 
@@ -19,12 +20,13 @@ function prepare_environment() {
   git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui.git ./$SD_PATH
 
   # Create virtual environment
-  python3 -m venv "$NOTEBOOK_PATH"/venv
+  /usr/local/bin/python3.10 -m venv "$NOTEBOOK_PATH"/"$VENV_PATH"
 
   # Install dependencies
-  source "$VENV_PATH"/bin/activate
-  pip install --pre torch torchvision --index-url https://download.pytorch.org/whl/nightly/rocm5.6
-  echo "Done."
+  source "$NOTEBOOK_PATH"/"$VENV_PATH"/bin/activate
+  pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+  pip install matplotlib-inline ipython
+
 }
 
 # Uninstall all
@@ -36,8 +38,22 @@ function uninstall() {
   sudo rm -rf $PROJECT_PATH
 }
 
+function make_python(){
+  PYTHON_VERSION="3.10.6"
+  cd $PROJECT_PATH
+
+  sudo apt -qq -y install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev liblzma-dev libsqlite3-dev wget libbz2-dev sudo apt python-is-python3
+  wget https://www.python.org/ftp/python/$PYTHON_VERSION/Python-$PYTHON_VERSION.tgz
+  tar -xvf Python-$PYTHON_VERSION.tgz
+  cd Python-$PYTHON_VERSION
+  sudo ./configure --enable-optimizations
+  sudo make -j$(nproc)
+  sudo make altinstall
+}
+
 
 # Exec
-#system_update
+system_update
+make_python
 prepare_environment
 
