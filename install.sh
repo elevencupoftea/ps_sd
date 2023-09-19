98a4d5ec860fdffd7b08133ee42763473c3aac69
elevencupoftea <elevencupoftea@gmail.com>
2023-09-19 08:32:50 +0300

    added python path

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ PROJECT_PATH="Stable"
 SD_PATH="stable_diffusion"
 NOTEBOOK_PATH=$(pwd)
 VENV_PATH=$PROJECT_PATH/venv
+PYTHON_PATH=$NOTEBOOK_PATH/$PROJECT_PATH/Python
 
 function show_status(){
   echo "-------------"
@@ -21,7 +22,7 @@ function system_update() {
 }
 
 function prepare_environment() {
-  show_status "Preapring environment"
+  show_status "Preapring environment. Be patient. This may take about 5 minutes"
   cd $PROJECT_PATH || exit
   # Clone Automatic1111's WebUI
   git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui.git ./$SD_PATH > /dev/null 2>&1
@@ -46,14 +47,14 @@ function uninstall() {
 }
 
 function make_python(){
-  show_status "Building Python"
-  PYTHON_VERSION="3.10.6"
+  show_status "Building Python. Be patient. This may take about 5 minutes"
+  PYTHON_VERSION="3.10.12"
 
   sudo apt -qq -y install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev liblzma-dev libsqlite3-dev wget libbz2-dev sudo apt python-is-python3 > /dev/null 2>&1
   wget https://www.python.org/ftp/python/$PYTHON_VERSION/Python-$PYTHON_VERSION.tgz > /dev/null 2>&1
   tar -xvf Python-$PYTHON_VERSION.tgz > /dev/null 2>&1
   cd Python-$PYTHON_VERSION 
-  sudo ./configure --enable-optimizations > /dev/null 2>&1
+  sudo ./configure --enable-optimizations --prefix=$PYTHON_PATH > /dev/null 2>&1
   sudo make -j$(nproc) > /dev/null 2>&1
   sudo make altinstall > /dev/null 2>&1
   cd ..
@@ -62,8 +63,8 @@ function make_python(){
 
 
 # Exec
-# system_update
-# make_python
+system_update
+make_python
 prepare_environment
 echo "######################################################"
 echo "----------------------ALL DONE------------------------"
