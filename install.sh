0a2f42297000f3f245ce86f4f49d48e35827862a
elevencupoftea <elevencupoftea@gmail.com>
2023-10-23 09:49:52 +0300

    xformers for pytorch 2.1.0

--- install.sh
+++ install.sh
@@ -35,6 +35,7 @@ function prepare_environment() {
   pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118 --no-cache-dir > /dev/null 2>&1
   pip install matplotlib-inline ipython tqdm requests > /dev/null 2>&1
   pip install -r "$SD_PATH"/requirements_versions.txt > /dev/null 2>&1
+  pip install xformers==0.0.23.dev662 > /dev/null 2>&1
 
 }
 
